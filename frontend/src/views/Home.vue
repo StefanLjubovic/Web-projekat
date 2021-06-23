43e90f38a7f85a2269dc4a469cbfcb0eae51d744
StefanLjubovic <ljubovicstefan@gmail.com>
2021-06-23 18:31:01 +0200

    Inicijalizacija frontenda

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <Login @login-user="loginUser"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Login from '@/components/Login.vue'
+
+export default {
+  watch:{
+    '$route'(to,from){
+    }
+  },
+  
+  name: 'Home',
+  components: {
+    Login
+  },
+  methods:{
+    loginUser(user){
+      console.log('aaa')
+      this.$router.push('/about')
+    }
+  }
+}
+</script>
